d15504aa0c70338ad70593803223c8d8f27f3435
Ali-Al-Ahmad <ali.alahmad.cs@gmail.com>
2024-11-13 22:17:20 +0200

    clean code

--- overlapping-intervals.py
+++ overlapping-intervals.py
@@ -1,14 +1,16 @@
+"this function merger overlapping intervals"
+
 array_of_intervals  = [[1,3], [2,4], [6,8]]
 #                     [[1, 4], [6,8]]
 
-def merge_intervals(intervals):
+def _merge_intervals(intervals):
     i=0
     while i<len(intervals)-1:
         j=i+1
         while j<len(intervals):
             print(intervals)
-            
-            # check if ther is overlapping set the new array and remove the unwanted inex
+
+            # check if their is overlapping set the new array and remove the unwanted inex
             if intervals[i][1] >= intervals[j][0] and intervals[i][1] <= intervals[j][1]:
                 if intervals[i][0]< intervals[j][0]:
                     intervals[j][0] = intervals[i][0]
@@ -19,8 +21,8 @@ def merge_intervals(intervals):
                     j+=1
             j+=1
         i+=1
-        
+
     return intervals
 
-intervals123 = merge_intervals(array_of_intervals)
+intervals123 = _merge_intervals(array_of_intervals)
 print(intervals123)
